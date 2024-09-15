cccba0c601d45b362748b79100bab7b0b7c942e1
Raveendra Soori <sooriravindra@gmail.com>
2024-09-15 18:33:16 +0530

    Add a convenience test script and ignore generated files

---
+++ 10/test.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+for jackfile in $(find . -name "*jack" -type f); do
+  xmlfile="${jackfile%.jack}.xml"
+  filename=$(basename "$jackfile")
+  prsfile="${filename%.jack}.prs"
+
+  ./analyzer.py $jackfile
+  ../tools/TextComparer.sh "${xmlfile}" "${prsfile}"
+done
